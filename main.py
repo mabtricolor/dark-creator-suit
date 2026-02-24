87213d3b24328f61c93a102179c0973898186598
mabtricolor <camarotespfc@gmail.com>
2026-02-24 18:49:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi.responses import FileResponse
 
 from rotas.ai_script_forge import router as script_forge_router
 from rotas.voice_synth import router as voice_router
+from fastapi.staticfiles import StaticFiles
+from rotas.tts_preview import router as tts_preview_router
 
 app = FastAPI(
     title="Dark Creator Suit",
@@ -16,13 +18,14 @@ app = FastAPI(
 # 1) Rotas (cada ferramenta em um arquivo separado)
 app.include_router(script_forge_router)
 app.include_router(voice_router)
+app.include_router(tts_preview_router)
 
 # 2) Frontend estático
 BASE_DIR = Path(__file__).resolve().parent
 FRONTEND_DIR = BASE_DIR / "frontend"
 STATIC_DIR = FRONTEND_DIR / "static"
 
-app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
+app.mount("/outputs", StaticFiles(directory=str(BASE_DIR / "outputs")), name="outputs")
 
 
 @app.get("/")
