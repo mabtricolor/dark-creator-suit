c09bbc3f8da23deaed8bd4505d00143258968aff
mabtricolor <camarotespfc@gmail.com>
2026-02-24 15:12:39 -0300

    Update app.js

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,6 +1,6 @@
 // GLOBALS
-const URL_API_VOZES = "https://suite.zappclube.com.br/vozes";
-const URL_API_DIRETA = "https://suite.zappclube.com.br/gerar_narracao";
+const URL_API_VOZES = "/vozes";
+const URL_API_DIRETA = "/gerar_narracao";
 let ideiasGlobais = []; 
 let srtGlobal = ""; 
 let vozSelecionada = "pt-BR-AntonioNeural"; 
